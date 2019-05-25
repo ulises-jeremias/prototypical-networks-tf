72b949e51283d21027fe5817c95f3fb397fbb8fc
ulises-jeremias <ulisescf.24@gmail.com>
2019-05-24 23:25:41 -0300

    Added cifar config

--- protonet/data/cifar10.py
+++ protonet/data/cifar10.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.utils import to_categorical
 
+
 class DataLoader(object):
     def __init__(self, data, n_classes, n_way, n_support, n_query):
         self.data = data
@@ -15,8 +16,8 @@ class DataLoader(object):
 
     def get_next_episode(self):
         n_examples = 20
-        support = np.zeros([self.n_way, self.n_support, 28, 28, 1], dtype=np.float32)
-        query = np.zeros([self.n_way, self.n_query, 28, 28, 1], dtype=np.float32)
+        support = np.zeros([self.n_way, self.n_support, 28, 28, 3], dtype=np.float32)
+        query = np.zeros([self.n_way, self.n_query, 28, 28, 3], dtype=np.float32)
         classes_ep = np.random.permutation(self.n_classes)[:self.n_way]
 
         for i, i_class in enumerate(classes_ep):
@@ -39,29 +40,23 @@ def load_cifar10(data_dir, config, splits):
 
     """
 
-    num_classes = 10
-
     # The data, split between train and test sets:
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    print('x_train shape:', x_train.shape)
-    print(x_train.shape[0], 'train samples')
-    print(x_test.shape[0], 'test samples')
 
     # Convert class vectors to binary class matrices.
-    y_train = to_categorical(y_train, num_classes)
-    y_test = to_categorical(y_test, num_classes)
-    print(y_train, 'y_train')
-    print(y_test, 'y_test')
+    y_train = to_categorical(y_train, 10)
+    y_test = to_categorical(y_test, 10)
 
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
+
     x_train /= 255
     x_test /= 255
 
-    print(x_train, 'x_train')
+    trainClasses, amountPerTrain = np.unique(y_train, return_counts=True)
+    testClasses, amountPerTest = np.unique(y_test, return_counts=True)
 
     ret = {}
-    """
     for split in splits:
         # n_way (number of classes per episode)
         if split in ['val', 'test']:
@@ -81,31 +76,25 @@ def load_cifar10(data_dir, config, splits):
         else:
             n_query = config['data.train_query']
 
-        # Get all class names
-        class_names = []
-        with open(os.path.join(split_dir, f"{split}.txt"), 'r') as f:
-            for class_name in f.readlines():
-                class_names.append(class_name.rstrip('\n'))
+        if split in ['val', 'test']:
+            y = y_test
+            x = x_test
+        else:
+            y = y_train
+            x = x_train
 
-        # Get class names, images paths and rotation angles per each class
-        class_paths, rotates = class_names_to_paths(data_dir,
-                                                    class_names)
-        classes, img_paths, rotates = get_class_images_paths(
-            class_paths,
-            rotates)
+        amountPerClass = amountPerTest if split in ['val', 'test'] else amountPerTrain
 
-        data = np.zeros([len(classes), len(img_paths[0]), 28, 28, 1])
-        for i_class in range(len(classes)):
-            for i_img in range(len(img_paths[i_class])):
-                data[i_class, i_img, :, :,:] = load_and_preprocess_image(
-                            img_paths[i_class][i_img], rotates[i_class])
+        i = np.argsort(y)
+        x = x[i, :, :, :]
+        data = np.reshape(x, (len(uniqueClasses), amountPerClass[0], 28, 28, 3))
 
         data_loader = DataLoader(data,
-                                 n_classes=,
+                                 n_classes=len(uniqueClasses),
                                  n_way=n_way,
                                  n_support=n_support,
                                  n_query=n_query)
 
         ret[split] = data_loader
-    """
+
     return ret
